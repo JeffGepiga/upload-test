4943f7b0956535fc3ba66179eecdc218b18acae5
Jeffrey Gepiga <51604875+JeffGepiga@users.noreply.github.com>
2024-06-25 22:31:02 +0800

    Updated

--- app/Http/Controllers/JqueryUploaderController.php
+++ app/Http/Controllers/JqueryUploaderController.php
@@ -8,9 +8,11 @@ use Illuminate\Http\Request;
 class JqueryUploaderController extends Controller
 {
     public function upload(Request $request) {
+        //validate
         $request->validate([
             'file'=>'required|mimes:mp4,ogx,oga,ogv,ogg,webm'
         ]);
+
         return (new JQueryUploadService)->ProcessUpload($request);
     }
 }
